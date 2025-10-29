d201db16b72cc7c01687d88752226946b5f96862
octomors <stepanka4444@gmail.com>
2025-10-29 16:36:43 +0700

    easter egg¯\_(ツ)_/

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './index.css';
+import { Fireworks } from 'fireworks-js';
+
+const fireworksContainer = document.querySelector('#sus');
+const fireworks = new Fireworks(fireworksContainer, {});
+
+const bookButton = document.querySelector('.footer__address-button');
+
+bookButton.addEventListener('click', () => {
+  fireworks.clear(); // очистить предыдущие
+
+  let count = 0;
+  const total = 8;
+  const interval = 200; // задержка между фейерверками в миллисекундах
+
+  const launchNext = () => {
+    if (count < total) {
+      fireworks.launch(1); // запустить один
+      count++;
+      setTimeout(launchNext, interval);
+    }
+  };
+
+  launchNext();
+});
